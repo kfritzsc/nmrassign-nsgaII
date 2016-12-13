c31f93434225f830cc2ab76c69d7ecd1b9f49586
Keith Fritzsching <kfritzsc@brandeis.edu>
2016-12-13 12:31:35 -0500

    New read and write methods for control files.
    Out and back test work.

--- nmrassign/fileio.py
+++ nmrassign/fileio.py
@@ -1,5 +1,6 @@
 """
-Functions for reading and writing files for the NMR GA-Assign program.
+Functions for reading and writing files for the NMR GA-Assign
+program.
 """
 import re
 import nmrassign.base
@@ -9,11 +10,10 @@ def read_sequence(seq_file):
     """
     Read sequence file.
 
-    :param seq_file: sequence file name with path. File should have a list
-        of 1-letter amino acid codes. FASTA type files are also supported but
-        if more then one sequence is in the file an error will be thrown.
-    :return: residue_sequence
-    :rtype: list
+    :param seq_file: sequence file name with path. File should have a
+        list of 1-letter amino acid codes. FASTA type files are also
+        supported but if more then one sequence is in the file an
+        error will be thrown.
     """
     sequence_count = 0
     residue_sequence = []
@@ -30,7 +30,7 @@ def read_sequence(seq_file):
 
             # if there has been more then one fasta header file
             if sequence_count > 1:
-                mesg = '{} contains more then one seq.'.format(seq_file)
+                mesg = '{} has more then one seq.'.format(seq_file)
                 raise ValueError(mesg)
             # there is not sequence info on this line skip it.
             continue
@@ -43,7 +43,7 @@ def write_sequence(seq, seq_file):
     """
     Write sequence to file.
 
-    :seq: iterable of 1-letter amino acids codes.
+    :param seq: iterable of 1-letter amino acids codes.
     :param seq_file: file object
     """
     if seq_file.name[-6:] == '.fasta':
@@ -65,7 +65,7 @@ def read_spectrum(spectrum_file):
     peak_groups = []
 
     # regex expression to match (and remove) things in parentheses
-    parentheses = re.compile('\(.+?\)')
+    remove_parentheses = re.compile(r'\(.+?\)')
 
     for line in spectrum_file:
         line = line.split()
@@ -78,9 +78,10 @@ def read_spectrum(spectrum_file):
         peaks = [(s, w) for (s, w) in zip(shifts, widths)]
         peaks = [nmrassign.base.Peak(*x) for x in peaks]
         num = line[-2]
-        assignments = parentheses.sub('', line[-1])
+        assignments = remove_parentheses.sub('', line[-1])
 
-        peak_group = nmrassign.base.PeakGroup(peaks, assignments, num)
+        peak_group = nmrassign.base.PeakGroup(peaks, assignments,
+                                              num)
         peak_groups.append(peak_group)
     return peak_groups
 
@@ -89,7 +90,7 @@ def write_spectrum(peak_groups, spectrum_file, deliminator=' '):
     """
     Write spectrum file.
 
-    :param peak_groups: list of base.PeakGroup
+    :param peak_groups: list of `base.PeakGroup`
     :param spectrum_file: file name with path.
     :param deliminator: delimiter default
     """
@@ -100,7 +101,8 @@ def write_spectrum(peak_groups, spectrum_file, deliminator=' '):
     spectrum_file.write('{} {}\n'.format(num_group, num_freq))
 
     for peak_group in peak_groups:
-        spectrum_file.write(peak_group.spectrum_line_str(deliminator))
+        spectrum_file.write(
+            peak_group.spectrum_line_str(deliminator))
         spectrum_file.write('\n')
 
 
@@ -109,13 +111,14 @@ def read_connection(connection_file):
     Read connection file.
 
     File Description:
-    The first line is count of atom lines. Each of the other lines has the
-    connection information for one atom. There are three piece of information
-    in each line.. The length of the line divided by 3.0 is the number of
-    spectra (n) that the atoms is in. The line[:n] is a list of spectra that
-    the atom is in. line[n:n * 2] is the atom index in the respective spectra.
-    line[-n:] says how the atoms connect to the surrounding residues. If 0 the
-    the convention is intra-residue.
+    The first line is count of atom lines. Each of the other lines
+    has the connection information for one atom. There are three
+    piece of information in each line. The length of the line divided
+    by 3.0 is the number of spectra (n) that the atom is in. The
+    line[:n] is a list of spectra that the atom is in. line[n:n * 2]
+    is the atom index in the respective spectra. line[-n:] says how
+    the atoms connect to the surrounding residues. If 0 the
+    connection is intraresidue.
 
     :param connection_file: file name with path.
     """
@@ -135,16 +138,19 @@ def read_connection(connection_file):
         spectra = [x - 1 for x in line[:n]]
         ids = [x - 1 for x in line[n:n * 2]]
         residue = line[-n:]
-        atoms.append(nmrassign.base.Connections(spectra, ids, residue))
+        atoms.append(nmrassign.base.Connections(spectra, ids,
+                                                residue))
     return atoms
 
 
-def write_connection(atom_connections, connections_file, delimiter=' '):
+def write_connection(atom_connections, connections_file,
+                     delimiter=' '):
     """
     Write connections file.
 
     :param atom_connections: list of base.PeakGroup
     :param connections_file: file name with path.
+    :param delimiter: type of delimiter
     """
     # Read the header line to get the number of peak and frequencies
     num_atoms = len(atom_connections)
@@ -158,3 +164,223 @@ def write_connection(atom_connections, connections_file, delimiter=' '):
         line += connects.residues
         connections_file.write(delimiter.join(map(str, line)))
         connections_file.write('\n')
+
+
+def read_control(control_file, parse_input=True):
+    """
+    Read control file
+
+    :param control_file: file name with path.
+    :param parse_input: Bool, if True parse input file.
+    """
+
+    params = dict()
+
+    params['method'] = next(control_file).split()[0]
+
+    next(control_file)
+    params['seq_file'] = next(control_file).split()[0]
+
+    if parse_input:
+        with open(params['seq_file'], 'r') as fid:
+            params['seq'] = read_sequence(fid)
+
+    params['n_spectra'] = int(next(control_file).split()[0])
+
+    # There are n_spectra spectrum files.
+    spectra_files = []
+    for _ in range(params['n_spectra']):
+        spectra_files.append(next(control_file).split()[0])
+    params['spectra_files'] = spectra_files
+
+    if parse_input:
+        spectra_peak_groups = []
+        for spectrum_file in params['spectra_files']:
+            with open(spectrum_file, 'r') as fid:
+                peak_groups = read_spectrum(fid)
+                spectra_peak_groups.append(peak_groups)
+        params['spectra_peak_groups'] = spectra_peak_groups
+
+    params['connection_file'] = next(control_file).split()[0]
+    if parse_input:
+        with open(params['connection_file'], 'r') as fid:
+            params['connections'] = read_connection(fid)
+
+    params['initial_file'] = next(control_file).split()[0]
+    if params['initial_file'].lower() in {'null', 'none'}:
+        params['initial_file'] = None
+    # TODO: Parse initial input file.
+
+    params['output_file'] = next(control_file).split()[0]
+    params['outtab_folder'] = next(control_file).split()[0]
+
+    # read all the algorithm parameters
+    next(control_file)
+
+    if params['method'].lower() == 'nsga2_assign':
+        params['group_size'] = int(next(control_file).split()[0])
+        params['pool_size'] = int(next(control_file).split()[0])
+        params['steps'] = int(next(control_file).split()[0])
+        params['nsga_attempts'] = int(next(control_file).split()[0])
+        params['free_steps'] = int(next(control_file).split()[0])
+        params['mutation_rate'] = float(
+            next(control_file).split()[0])
+        params['additional_rate'] = float(
+            next(control_file).split()[0])
+        params['crossover_rate'] = float(
+            next(control_file).split()[0])
+        params['null_prob'] = float(next(control_file).split()[0])
+        line = next(control_file).split()[0]
+        if line[0] == '!':
+            params['random_seed'] = None
+        else:
+            params['random_seed'] = int(line)
+
+    elif params['method'].lower() == 'nsga2_mc_assign':
+        params['group_size'] = int(next(control_file).split()[0])
+        params['pool_size'] = int(next(control_file).split()[0])
+        params['steps'] = int(next(control_file).split()[0])
+        params['nsga_attempts'] = int(next(control_file).split()[0])
+        params['mc_attempts'] = int(next(control_file).split()[0])
+        params['free_steps'] = int(next(control_file).split()[0])
+        params['mutation_rate'] = float(
+            next(control_file).split()[0])
+        params['additional_rate'] = float(
+            next(control_file).split()[0])
+        params['crossover_rate'] = float(
+            next(control_file).split()[0])
+        params['null_prob'] = float(next(control_file).split()[0])
+        params['w1_min'] = float(next(control_file).split()[0])
+        params['w2_min'] = float(next(control_file).split()[0])
+        params['w3_min'] = float(next(control_file).split()[0])
+        params['w4_min'] = float(next(control_file).split()[0])
+        params['w1_max'] = float(next(control_file).split()[0])
+        params['w2_max'] = float(next(control_file).split()[0])
+        params['w3_max'] = float(next(control_file).split()[0])
+        params['w4_max'] = float(next(control_file).split()[0])
+        line = next(control_file).split()[0]
+        if line != '!':
+            params['random_seed'] = line
+        else:
+            params['random_seed'] = None
+    else:
+        msg = 'Method {} not NSGA2_ASSIGN or NSGA2_MC_ASSIGN'.format(
+            params['method'])
+        raise ValueError(msg)
+    return params
+
+
+def write_control(params, control_file, write_input=False):
+    """
+    Write the control file from the params dictionary. Optionally,
+    write all input files.
+
+    :param control_file: file name with path.
+    :param params: dictionary with all info to write to file(s)
+    :param write_input: default False, write all out put file from
+        param data. The directory will be the basename of the
+        control_file.
+    """
+
+    control_file.write(params['method'] + ' input\n')
+
+    # write file parameters
+    control_file.write('!' + '*' * 20 + '\n')
+    control_file.write(params['seq_file'] + '\t! seq_file\n')
+    if write_input:
+        with open(params['seq_file'], 'w') as fid:
+            write_sequence(params['seq'], fid)
+
+    control_file.write(str(params['n_spectra']) + '\t! seq_file\n')
+
+    for spectrum_file in params['spectra_files']:
+        control_file.write(spectrum_file + '\t! spectrum_file\n')
+
+    if write_input:
+        for n in params['n_spectra']:
+            with open(params['spectra_files'][n], 'w') as fid:
+                write_spectrum(
+                    params['spectra_peak_groups'][n], fid)
+
+    control_file.write(
+        params['connection_file'] + '\t! connection file\n')
+    if write_input:
+        with open(params['connection_file'], 'w') as fid:
+            write_connection(params['connections'], fid)
+
+    if not params['initial_file']:
+        control_file.write('NULL' + '\t! initial file\n')
+    else:
+        control_file.write(
+            params['initial_file'] + '\t! initial state file\n')
+    # TODO: Output initial input state file.
+
+    control_file.write(params['output_file'] + '\t! output file\n')
+    control_file.write(
+        params['outtab_folder'] + '\t! outtab folder\n')
+
+    # Write the rest of the algorithm parameters
+    control_file.write('!' + '*' * 20 + '\n')
+    if params['method'].lower() == 'nsga2_assign':
+        control_file.write(
+            str(params['group_size']) + '\t! group size\n')
+        control_file.write(
+            str(params['pool_size']) + '\t! pool size\n')
+        control_file.write(
+            str(params['steps']) + '\t! steps\n')
+        control_file.write(
+            str(params['nsga_attempts']) + '\t! steps\n')
+        control_file.write(
+            str(params['free_steps']) + '\t! free steps\n')
+        control_file.write(
+            str(params['mutation_rate']) + '\t! mutation_rate\n')
+        control_file.write(
+            str(params['additional_rate']) + '\t! additional rate\n')
+        control_file.write(
+            str(params['crossover_rate']) + '\t! crossover rate\n')
+        control_file.write(
+            str(params['null_prob']) + '\t! null prob\n')
+
+        if params['random_seed']:
+            control_file.write(
+                str(params['random_seed']) + '\t! random seed\n')
+        control_file.write('!' + '*' * 20 + '\n')
+
+    elif params['method'].lower() == 'nsga2_mc_assign':
+        params['group_size'] = next(control_file).split()[0]
+        params['pool_size'] = next(control_file).split()[0]
+        params['steps'] = next(control_file).split()[0]
+        params['nsga_attempts'] = next(control_file).split()[0]
+        params['mc_attempts'] = next(control_file).split()[0]
+        params['free_steps'] = next(control_file).split()[0]
+        params['mutation_rate'] = next(control_file).split()[0]
+        params['additional_rate'] = next(control_file).split()[0]
+        params['crossover_rate'] = next(control_file).split()[0]
+        params['null_prob'] = next(control_file).split()[0]
+        params['w1_min'] = next(control_file).split()[0]
+        params['w2_min'] = next(control_file).split()[0]
+        params['w3_min'] = next(control_file).split()[0]
+        params['w4_min'] = next(control_file).split()[0]
+        params['w1_max'] = next(control_file).split()[0]
+        params['w2_max'] = next(control_file).split()[0]
+        params['w3_max'] = next(control_file).split()[0]
+        params['w4_max'] = next(control_file).split()[0]
+        line = next(control_file).split()[0]
+        if line != '!':
+            params['random_seed'] = line
+        else:
+            params['random_seed'] = None
+        control_file.write('!' + '*' * 20 + '\n')
+
+
+def read_outtab(outtab_file):
+    """
+    Read output table file.
+
+    :param outtab_file: file name with path.
+    """
+    header = next(outtab_file)
+    values = map(int, header.split()[-5:])
+    score = nmrassign.base.Score(*values)
+
+    return score
--- tests/test_fileio.py
+++ tests/test_fileio.py
@@ -14,7 +14,8 @@ class FileIOTest(unittest.TestCase):
     """
     Test `nmrassign.fileio.` read and write functions.
 
-    Use nearly ideal GB1 data to test reading and writing files for GA assign.
+    Use nearly ideal GB1 data to test reading and writing files for
+    GA assign.
     """
     def setUp(self):
 
@@ -32,13 +33,27 @@ class FileIOTest(unittest.TestCase):
         self.gb1_seq_fasta = os.path.join(test_data_dir, seq_file)
 
         seq_file = os.path.join('data', 'gb1_1', 'bad_seq.fasta')
-        self.gb1_bad_seq_fasta = os.path.join(test_data_dir, seq_file)
+        self.gb1_bad_seq_fasta = os.path.join(test_data_dir,
+                                              seq_file)
 
         # other files.
         gb1_spec_file_1 = os.path.join('data', 'gb1_1', 'NCACX.txt')
-        self.gb1_spec_1 = os.path.join(test_data_dir, gb1_spec_file_1)
-        gb1_con_file = os.path.join('data', 'gb1_1', 'connection.txt')
+        self.gb1_spec_1 = os.path.join(test_data_dir,
+                                       gb1_spec_file_1)
+        gb1_con_file = os.path.join('data', 'gb1_1',
+                                    'connection.txt')
         self.gb1_con = os.path.join(test_data_dir, gb1_con_file)
+
+        # control file
+        control_nsga_file = os.path.join('data', 'gb1_1',
+                                         'control_nsga.txt')
+        self.gb1_control_nsga_file = os.path.join(test_data_dir,
+                                                  control_nsga_file)
+
+        # output files
+        gb1_tab_file = os.path.join('data', 'gb1_1', 'output',
+                                    'outtab001.txt')
+        self.gb1_tab_file = os.path.join(test_data_dir, gb1_tab_file)
 
     def tearDown(self):
         # Remove the directory after the test.
@@ -64,8 +79,8 @@ class FileIOTest(unittest.TestCase):
         """
         Read_sequence for a fasta file with multiple sequences.
 
-        In this case it is not known which sequence should be used so a
-        ValueError should be thrown.
+        In this case it is not known which sequence should be used
+        so a ValueError should be thrown.
         """
         with open(self.gb1_bad_seq_fasta, 'r') as fid:
             self.assertRaises(ValueError, fileio.read_sequence, fid)
@@ -76,10 +91,12 @@ class FileIOTest(unittest.TestCase):
         """
         sequence = fileio.read_sequence(self.gb1_seq)
 
-        with open(os.path.join(self.test_dir, 'test_seq.fasta'), 'w') as fid:
+        with open(os.path.join(self.test_dir, 'test_seq.fasta'),
+                  'w') as fid:
             fileio.write_sequence(sequence, fid)
 
-        with open(os.path.join(self.test_dir, 'test_seq.fasta'), 'r') as fid:
+        with open(os.path.join(self.test_dir, 'test_seq.fasta'),
+                  'r') as fid:
             read_seq = fileio.read_sequence(fid)
 
         self.assertEqual(sequence, read_seq)
@@ -100,13 +117,16 @@ class FileIOTest(unittest.TestCase):
         with open(self.gb1_spec_1, 'r') as fid:
             groups = fileio.read_spectrum(fid)
 
-        with open(os.path.join(self.test_dir, 'test_spec_1.txt'), 'w') as fid:
+        with open(os.path.join(self.test_dir, 'test_spec_1.txt'),
+                  'w') as fid:
             fileio.write_spectrum(groups, fid)
 
-        with open(os.path.join(self.test_dir, 'test_spec_1.txt'), 'r') as fid:
+        with open(os.path.join(self.test_dir, 'test_spec_1.txt'),
+                  'r') as fid:
             new_groups = fileio.read_spectrum(fid)
 
-        self.assertEqual(groups[10].assignments, new_groups[10].assignments)
+        self.assertEqual(groups[10].assignments,
+                         new_groups[10].assignments)
 
     def test_read_connection(self):
         """
@@ -125,14 +145,49 @@ class FileIOTest(unittest.TestCase):
         with open(self.gb1_con, 'r') as fid:
             connections = fileio.read_connection(fid)
 
-        with open(os.path.join(self.test_dir, 'test_conn.txt'), 'w') as fid:
+        with open(os.path.join(self.test_dir, 'test_conn.txt'),
+                  'w') as fid:
             fileio.write_connection(connections, fid)
 
-        with open(os.path.join(self.test_dir, 'test_conn.txt'), 'r') as fid:
+        with open(os.path.join(self.test_dir, 'test_conn.txt'),
+                  'r') as fid:
             new_connections = fileio.read_connection(fid)
 
         self.assertEqual(connections, new_connections)
 
+    def test_read_control_spot1(self):
+        """
+        Read control file.
+        """
+        with open(self.gb1_control_nsga_file, 'r') as fid:
+            params = fileio.read_control(fid)
+            self.assertEqual(params['steps'], 20)
+
+    def test_read_control_spot2(self):
+        """
+        Read control file.
+        """
+        with open(self.gb1_control_nsga_file, 'r') as fid:
+            params = fileio.read_control(fid)
+            self.assertEqual(params['random_seed'], 1)
+            self.assertEqual(params['random_seed'], 1)
+
+    def test_control_round_trip(self):
+        """
+        Read control file , write control, read control compare.
+        """
+        with open(self.gb1_control_nsga_file, 'r') as fid:
+            params = fileio.read_control(fid, parse_input=False)
+
+        with open(os.path.join(self.test_dir, 'test_control.txt'),
+                  'w') as fid:
+            fileio.write_control(params, fid, write_input=False)
+
+        with open(os.path.join(self.test_dir, 'test_control.txt'),
+                  'r') as fid:
+            read_params = fileio.read_control(fid, parse_input=False)
+
+        self.assertEqual(params, read_params)
 
 if __name__ == "__main__":
     unittest.main()
